57df0b79dbc667367880debc1ebdb9708e18ba19
Joel La Russa <matrixfox@gmail.com>
2013-10-16 15:47:01 -0700

    GroundworkCSS 2 Footer Added

--- footer.php
+++ footer.php
@@ -9,11 +9,56 @@
     <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/groundwork.all.js"></script>
     <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/script.js"></script>
 
-<footer id="footer" role="contentinfo">
-<div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date('Y'), esc_html(get_bloginfo('name')) ); echo sprintf( __( ' Theme By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
-</div>
+<footer id="footer" class="gap-top bounceInUp animated" role="contentinfo">
+  <div class="box square charcoal">
+    <div class="container padded">
+      <div class="row">
+        <div class="one small-tablet fourth padded">
+          <h5 class="green">Example Layouts</h5>
+          <ul class="unstyled">
+            <li><a href="./layout-a.html">Web Page</a></li>
+            <li><a href="./layout-b.html">Image Gallery</a></li>
+            <li><a href="./layout-c.html">Ecommerce Page</a></li>
+            <li><a href="./layout-d.html">Contact Page</a></li>
+          </ul>
+        </div>
+        <div class="three small-tablet fourths padded">
+          <h5 class="blue">Documentation</h5>
+          <ul class="unstyled three-column two-column-mobile">
+            <li><a href="./grid.html" title="Responsive grid system, grid adapters and helpers">Grid</a></li>
+            <li><a href="./helpers.html" title="Layout helpers, spinners and much more">Helpers</a></li>
+            <li><a href="./typography.html" title="Text elements, quotes, code and web fonts">Typography</a></li>
+            <li><a href="./navigation.html" title="Navigation">Navigation</a></li>
+            <li><a href="./buttons.html" title="Buttons, button groups, button menus">Buttons</a></li>
+            <li><a href="./boxes.html" title="Boxes">Boxes</a></li>
+            <li><a href="./messages.html" title="Message boxes">Message Boxes</a></li>
+            <li><a href="./tables.html" title="Tables">Tables</a></li>
+            <li><a href="./tabs.html" title="Tabs">Tabs</a></li>
+            <li><a href="./forms.html" title="Form elements">Form Elements</a></li>
+            <li><a href="./icons.html" title="Icons">Icons</a></li>
+            <li><a href="./responsive-text.html" title="Responsive text and multi-line text block truncation">Responsive Text</a></li>
+            <li><a href="./placeholder-text.html" title="Placeholder text and placeholder fonts for rapid prototyping and wireframes">Placeholder Text</a></li>
+            <li><a href="./animations.html" title="Pure CSS3 Animations">Animations</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="box square">
+    <div class="container padded">
+      <div class="row">
+        <div class="one half padded copyright">
+          <p>© <a href="#">matrixfox</a> 2013</p>
+          <p class="icon-long-arrow-up icon-1x"> <a class="backtotop" href='#'>Back to top</a></p>
+        </div>
+        <div class="one half padded">
+          <p class="large padded align-right align-center-small-tablet"><a href="http://twitter.com/groundworkcss" target="_blank" title="@groundworkcss" style="text-decoration:none;" class="large inline gapped"></a><a href="http://github.com/groundworkcss" target="_blank" title="@groundworkcss" style="text-decoration:none;" class="large inline gapped"></a><a href="mailto:groundworkcss@gmail.com" title="groundworkcss@gmail.com" style="text-decoration:none;" class="large inline gapped"></a></p>
+        </div>
+      </div>
+    </div>
+  </div>
 </footer>
+
 </div>
 <?php wp_footer(); ?>
 </body>
