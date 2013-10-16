fd63af15741ea74f90dc24ea34aa838c9b42776d
Joel La Russa <matrixfox@gmail.com>
2013-10-16 16:48:46 -0700

    Added Single and Page Class

--- single.php
+++ single.php
@@ -1,5 +1,5 @@
 <?php get_header(); ?>
-<section id="content" role="main">
+<section id="content" class="padded bounceInDown animated" role="main">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <?php get_template_part('entry'); ?>
 <?php if ( ! post_password_required() ) comments_template('', true); ?>
