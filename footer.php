dc8831b0dbb6ae7ac5a99135bee0d07d61bfe7c6
Joel La Russa <matrixfox@gmail.com>
2013-10-16 15:23:29 -0700

    Injected GroundworkCSS in Blankslate

--- footer.php
+++ footer.php
@@ -1,4 +1,14 @@
 </div>
+
+    <!-- Modernizr -->
+    <script src="<?php bloginfo('stylesheet_directory'); ?>/js/libs/modernizr-2.6.2.min.js"></script>
+    <!-- jQuery-->
+    <script src="../../js/jquery.js"></script>
+    <!--<script src='https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js' type='text/javascript'></script>-->
+    <!-- javascript-->
+    <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/groundwork.all.js"></script>
+    <script type="text/javascript" src="<?php bloginfo('stylesheet_directory'); ?>/js/script.js"></script>
+
 <footer id="footer" role="contentinfo">
 <div id="copyright">
 <?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date('Y'), esc_html(get_bloginfo('name')) ); echo sprintf( __( ' Theme By: %1$s.', 'blankslate' ), '<a href="http://tidythemes.com/">TidyThemes</a>' ); ?>
--- header.php
+++ header.php
@@ -2,8 +2,21 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo('charset'); ?>" />
-<meta name="viewport" content="width=device-width" />
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+
 <title><?php wp_title(' | ', true, 'right'); ?></title>
+
+<!-- framework css --><!--[if gt IE 9]><!-->
+<link type="text/css" rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/groundwork.css"><!--<![endif]--><!--[if lte IE 9]>
+<link type="text/css" rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/groundwork-core.css">
+<link type="text/css" rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/groundwork-type.css">
+<link type="text/css" rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/groundwork-ui.css">
+<link type="text/css" rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/groundwork-anim.css">
+<link type="text/css" rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/css/groundwork-ie.css"><![endif]-->
+
+<!-- Master Stylesheet -->
 <link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_uri(); ?>" />
 <?php wp_head(); ?>
 </head>
@@ -14,11 +27,11 @@
 <div id="site-title"><?php if ( ! is_singular() ) {echo '<h1>';} ?><a href="<?php echo esc_url(home_url('/')); ?>" title="<?php esc_attr_e( get_bloginfo('name'), 'blankslate' ); ?>" rel="home"><?php echo esc_html( get_bloginfo('name') ); ?></a><?php if ( ! is_singular() ) {echo '</h1>';} ?></div>
 <div id="site-description"><?php bloginfo('description'); ?></div>
 </section>
-<nav id="menu" role="navigation">
-<div id="search">
-<?php get_search_form(); ?>
-</div>
-<?php wp_nav_menu( array( 'theme_location' => 'main-menu' ) ); ?>
+<nav role="navigation" class="nav">
+  <ul role="menubar">
+    <li><a href="http://localhost/wordpress/"><i class="icon-home"></i> Home</a></li>
+    <?php wp_list_pages('title_li=' ); ?>
+  </ul>
 </nav>
 </header>
-<div id="container">
+<div class="container">
