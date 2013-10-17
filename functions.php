992f9005718476678a6ac1d5e9d6701d3dc7d9e3
Joel La Russa <matrixfox@gmail.com>
2013-10-17 13:16:24 -0700

    Added Custom Excerpt

--- functions.php
+++ functions.php
@@ -35,6 +35,24 @@ return 'http://placehold.it/750x400/2ecc71/ffffff/&amp;text=Default+Image';
 }
 
 
+function custom_excerpt_length( $length ) {
+	return 64;
+}
+add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+
+
+function new_excerpt_more( $more ) {
+	return '...';
+}
+add_filter('excerpt_more', 'new_excerpt_more');
+
+
+function excerpt_read_more_link($output) {
+ global $post;
+ return $output . '<a href="'. get_permalink($post->ID) . '"><button class="gap-bottom asphalt">Read More</button></a>';
+}
+add_filter('the_excerpt', 'excerpt_read_more_link');
+
 
 
 
