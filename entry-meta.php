b2827c24fc0ff5e6726d92525825404d459a4ae7
Joel La Russa <matrixfox@gmail.com>
2013-10-17 14:21:37 -0700

    Added Gap on entry-meta

--- entry-meta.php
+++ entry-meta.php
@@ -1,4 +1,4 @@
-<section class="entry-meta">
+<section class="entry-meta gap-top gap-bottom">
 <span class="author vcard"><?php the_author_posts_link(); ?></span>
 <span class="meta-sep"> | </span>
 <span class="entry-date"><?php the_time( get_option('date_format') ); ?></span>
